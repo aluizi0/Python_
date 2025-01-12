2a74c9e8b3e8afad218dafdbe79a13a523d4beff
Aluízio Oliveira Gonçalves Filho <80135392+aluizi0@users.noreply.github.com>
2025-01-12 13:57:46 -0300

    print, tipos, input e concatenação

---
+++ 1-Python/3-input.py
@@ -0,0 +1,13 @@
+'''
+Comentários para explicar o código
+'''
+
+nome = input('Qual o nome do Jogo?\n')
+yearLaunch = int(input('Qual o ano de lançamento?\n'))
+gamerPrice = float(input('Qual o preço do jogo?\n'))
+planIncluded = input('O jogo possui plano mensal incluso?\n')
+
+print(nome)
+print(yearLaunch)
+print(gamerPrice)
+print(planIncluded)
