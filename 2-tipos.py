df4b7c45faea2d073a8e7a5073c93028215459d1
Aluizi0 <you@example.com>
2025-01-17 08:39:37 -0300

    operadores

---
+++ 2-tipos.py
@@ -0,0 +1,14 @@
+name = 'super mario'
+yearLaunch = 1985
+gamePrice = 250.00
+planIncluded = False
+
+print(name)
+print(yearLaunch)
+print(gamePrice)
+print(planIncluded)
+
+print(type(name))
+print(type(yearLaunch))
+print(type(gamePrice))
+print(type(planIncluded))
