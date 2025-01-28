118843ab2fa8d606c1e636328683341348f147cc
Aluízio Oliveira Gonçalves Filho <80135392+aluizi0@users.noreply.github.com>
2025-01-28 19:15:30 -0300

    exercicio1

---
+++ 1-Python/exercicio1.py
@@ -0,0 +1,15 @@
+# Desafio 1
+numero = int(input('Digite um número: '))
+sucessor = numero + 1
+antecessor = numero - 1
+print(f'O sucessor de {numero} é {sucessor} e o antecessor é {antecessor}')
+
+# Desafio 2
+
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+nota3 = float(input('Digite a terceira nota: '))
+nota4 = float(input('Digite a quarta nota: '))
+
+media = (nota1 + nota2 + nota3 + nota4) / 4
+print(f'A média das notas é: {media}')
