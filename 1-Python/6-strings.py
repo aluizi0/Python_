cec08c48c9945dd16ed08d224f71b19522d12c0a
Aluízio Oliveira Gonçalves Filho <80135392+aluizi0@users.noreply.github.com>
2025-01-28 19:26:29 -0300

    strings

---
+++ 1-Python/6-strings.py
@@ -0,0 +1,13 @@
+gameName = 'FreeFire'
+gameName2 = 'freefire'
+
+print(gameName == gameName2) # False, Python é case sensitive
+
+gameDescription = '''
+    Free Fire é um jogo eletrônico mobile de ação-aventura 
+    do gênero battle royale, desenvolvido pela 111dots Studio
+    e publicado pela Garena.
+'''
+
+print(gameDescription)
+
