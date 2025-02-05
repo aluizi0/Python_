960498feffba83cc1848d102e40d94d169e15a63
Aluizi0 <you@example.com>
2025-02-05 12:41:41 -0300

    fatiamento de strings

---
+++ 1-Python/exercicio2.py
@@ -0,0 +1,18 @@
+#Substituindo caractere repetido
+
+name = input('Digite o Nome do Jogo:')
+
+char = name[0].lower()
+new_name = name.replace(char, '$')
+new_name = char + new_name[1:]
+print(new_name)
+
+#Troca de caracteres
+
+st1 = 'cab' #zyb
+st2 = 'zyx' #cax
+
+new_st1 = st2[:2] + st1[2:]
+new_st2 = st1[:2] + st2[2:]
+print(new_st1)
+print(new_st2)
