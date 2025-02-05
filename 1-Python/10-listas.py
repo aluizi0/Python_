5cded36d28c3e38f04ac5b1002a3dd0d2ac6280a
Aluizi0 <you@example.com>
2025-02-05 12:48:44 -0300

    Tipo de Dado Lista

---
+++ 1-Python/10-listas.py
@@ -0,0 +1,17 @@
+gameFifa = ['Fifa23', 2022, 90.50, True]
+print(gameFifa)
+
+gamesList = ['Valorant', 'Lolzin', 'Pes 25', 'cod']
+print(gamesList)
+
+# 1 - Busque os dois primeiros itens da lista
+print(gamesList[0:2])
+
+# 2 - Busque o último item da lista
+print(gamesList[-1])
+
+# 3 - Busca jogos até uma posição
+print(gamesList[:3])
+
+# 4 - Busca jogos de uma posição em diante
+print(gamesList[1:])
