dde0bb0cdb10dca7573e86ad876260185f40a93b
Aluizi0 <you@example.com>
2025-02-06 12:59:31 -0300

    tipo de dado set

---
+++ 1-Python/13-set.py
@@ -0,0 +1,28 @@
+gamesSet = {
+    'mario', 'pokemon', 'fifa',
+    'valorant', 'lol', 'truco',
+}
+
+print(gamesSet)
+
+#tamanho
+print(len(gamesSet))
+
+# 1 - True e 1 são considerados o mesmo valor
+exampleSet = {"lol", True, 1, 90.50} 
+print(exampleSet)
+
+# 2 - Adicionando item no set
+gamesSet.add("Resident Evil 4")
+print(gamesSet)
+
+# 3 - Adicionando item de outro set
+gamesSet.update(exampleSet)
+print(gamesSet)
+
+# 4 - Remove um item no set
+gamesSet.remove(True)
+gamesSet.remove(90.5)
+print(gamesSet)
+
+# 5 - Não possibilita recuperar valores no set via slice
